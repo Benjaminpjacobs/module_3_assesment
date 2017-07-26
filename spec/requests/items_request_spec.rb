bcbe2484a8b6d188030ff9e1bed79ed3e50fb725
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-26 12:15:26 -0600

    adds status code check for get requests

--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe "Items Requests" do
       item = items.first
 
       expect(response).to be_success
+      expect(response.status).to be(200)
       expect(items.count).to eq(5)
       expect(item).to have_key(:id)
       expect(item).to have_key(:name)
@@ -25,6 +26,7 @@ RSpec.describe "Items Requests" do
     item = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_success
+    expect(response.status).to be(200)
     expect(item).to have_key(:id)
     expect(item).to have_key(:name)
     expect(item).to have_key(:description)
@@ -64,27 +66,3 @@ RSpec.describe "Items Requests" do
   end
 end
 
-
-
-
-
-
-# For this challenge clone [Storedom](https://github.com/turingschool-examples/storedom).
-
-# We need an API for the application that can both read and write data. Start by focusing on functionality for items. All of this should happen in a dedicated, versioned controller.
-
-# When I send a GET request to `/api/v1/items`
-# I receive a 200 JSON response containing all items
-# And each item has an id, name, description, and image_url but not the created_at or updated_at
-
-# When I send a GET request to `/api/v1/items/1`
-# I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
-
-# When I send a DELETE request to `/api/v1/items/1`
-# I receive a 204 JSON response if the record is successfully deleted
-
-# When I send a POST request to `/api/v1/items` with a name, description, and image_url
-# I receive a 201 JSON  response if the record is successfully created
-# And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
-
-# * Verify that your non-GET requests work using Postman or curl. (Hint: `ActionController::API`). Why doesn't the default `ApplicationController` support POST and PUT requests?
